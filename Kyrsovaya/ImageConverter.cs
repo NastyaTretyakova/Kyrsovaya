8dc32ed321246d75c99cfbc686f4771ef61e174a
Анастасия Третьякова <nastena.91192@gmail.com>
2018-11-17 21:48:39 +0300

    Fix Img

--- Kyrsovaya/ImageConverter.cs
+++ Kyrsovaya/ImageConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -7,16 +9,28 @@ namespace Kyrsovaya
 {
     public class ImageConverter : IValueConverter
     {
-        public object Convert(
-            object value, Type targetType, object parameter, CultureInfo culture)
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new BitmapImage(new Uri(value.ToString()));
+            Uri uriSource = new Uri(System.IO.Path.Combine(GetExeDirectory(), value.ToString()));
+            return new BitmapImage(uriSource);
         }
+
 
         public object ConvertBack(
             object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotSupportedException();
+        }
+
+
+        private string GetExeDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            path = Path.GetDirectoryName(path);
+            return path;
         }
     }
 }
