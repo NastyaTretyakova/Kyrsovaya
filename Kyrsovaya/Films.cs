c8e540628043022871aa4faccf6176241740ea6f
Анастасия Третьякова <nastena.91192@gmail.com>
2018-11-17 18:21:06 +0300

    AddFilm

---
+++ Kyrsovaya/Films.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyrsovaya
+{
+    public class Films
+    {
+        public string Name;
+        public string Genre;
+        public int Year;
+        public string Description;
+        public string ImagePath;
+    }
+}
