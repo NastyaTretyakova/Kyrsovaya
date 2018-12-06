670d2d30cd70607a720b3e9ccb4fa94732bd69fd
Анастасия Третьякова <nastena.91192@gmail.com>
2018-12-06 18:04:56 +0300

    с

--- Kyrsovaya/MainWindow.xaml.cs
+++ Kyrsovaya/MainWindow.xaml.cs
@@ -18,8 +18,6 @@ namespace Kyrsovaya
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
-        //string topList = string.Empty;
-
         public string TopList { get; set; } = string.Empty;
         public event PropertyChangedEventHandler PropertyChanged;
         private List<Films> catalogSourse = new List<Films>();
@@ -43,21 +41,17 @@ namespace Kyrsovaya
 
         public MainWindow()
         {
-            //this.catalog.Add(new Films() { Name = "", Genre = new List<string>(){"" }, Year = , ImagePath = "Res\\MissionImpossibleFallout.jpg", Description = ""});
-            this.catalogSourse.Add(new Films() { Name = "Мег: Монстр глубины", Genre = new List<string>() { "ужасы", "ужасы" }, Year = 2019, ImagePath = "Res\\TheMeg.jpg", Description = "Глубоководный батискаф, осуществляющий наблюдение в рамках международной программы по изучению подводной жизни, был атакован огромным существом, которое все считали давно вымершим. Неисправный аппарат теперь лежит на дне глубочайшей впадины Тихого океана… с оказавшимся в ловушке экипажем. Их время на исходе. Китайский океанограф-новатор, несмотря на протесты его дочери Суинь, зовет спасателя-подводника Джонаса Тейлора, чтобы тот помог спасти команду и океан от невиданной угрозы — доисторической 23-метровой акулы, мегалодона. Никто и подумать не мог, что много лет назад Тейлор уже сталкивался с этим чудовищным созданием." });
+          
+            this.catalogSourse.Add(new Films() { Name = "Мег: Монстр глубины", Genre = new List<string>() { "ужасы" }, Year = 2019, ImagePath = "Res\\TheMeg.jpg", Description = "Глубоководный батискаф, осуществляющий наблюдение в рамках международной программы по изучению подводной жизни, был атакован огромным существом, которое все считали давно вымершим. Неисправный аппарат теперь лежит на дне глубочайшей впадины Тихого океана… с оказавшимся в ловушке экипажем. Их время на исходе. Китайский океанограф-новатор, несмотря на протесты его дочери Суинь, зовет спасателя-подводника Джонаса Тейлора, чтобы тот помог спасти команду и океан от невиданной угрозы — доисторической 23-метровой акулы, мегалодона. Никто и подумать не мог, что много лет назад Тейлор уже сталкивался с этим чудовищным созданием." });
             this.catalogSourse.Add(new Films() { Name = "Миссия невыполнима: Последствия", Genre = new List<string>() { "боевик", "ужасы" }, Year = 2018, ImagePath = "Res\\MissionImpossibleFallout.jpg", Description = "Итан Хант и его команда, а также недавно примкнувшие к ним союзники, вынуждены действовать наперегонки со временем, когда новая миссия идет не по плану." });
             this.catalogSourse.Add(new Films() { Name = "Побег из Шоушенка", Genre = new List<string>(){ "драма" }, Year = 1994, ImagePath = "Res\\TheShawshankRedemption.jpg", Description = "Бухгалтер Энди Дюфрейн обвинен в убийстве собственной жены и ее любовника. Оказавшись в тюрьме под названием Шоушенк, он сталкивается с жестокостью и беззаконием, царящими по обе стороны решетки. Каждый, кто попадает в эти стены, становится их рабом до конца жизни. Но Энди, обладающий живым умом и доброй душой, находит подход как к заключенным, так и к охранникам, добиваясь их особого к себе расположения." });
             this.catalogSourse.Add(new Films() { Name = "Форрест Гамп", Genre = new List<string>() { "драма", "мелодрама" }, Year = 1994, ImagePath = "Res\\ForrestGump.jpg", Description = "От лица главного героя Форреста Гампа, слабоумного безобидного человека с благородным и открытым сердцем, рассказывается история его необыкновенной жизни.Фантастическим образом превращается он в известного футболиста, героя войны, преуспевающего бизнесмена.Он становится миллиардером, но остается таким же бесхитростным, глупым и добрым.Форреста ждет постоянный успех во всем, а он любит девочку, с которой дружил в детстве, но взаимность приходит слишком поздно." });
             this.catalogSourse.Add(new Films() { Name = "Зеленая миля", Genre = new List<string>(){ "фэнтези", "драма", "криминал", "детектив" }, Year = 1999, ImagePath = "Res\\TheGreenMile.jpg", Description = "Пол Эджкомб — начальник блока смертников в тюрьме «Холодная гора», каждый из узников которого однажды проходит «зеленую милю» по пути к месту казни. Пол повидал много заключённых и надзирателей за время работы. Однако гигант Джон Коффи, обвинённый в страшном преступлении, стал одним из самых необычных обитателей блока."});
-             this.catalogSourse.Add(new Films() { Name = "Список Шиндлера", Genre = new List<string>(){ "драма", "биография", "история" }, Year = 1993 , ImagePath = "Res\\Schindler'sList.jpg", Description = "Фильм рассказывает реальную историю загадочного Оскара Шиндлера, члена нацистской партии, преуспевающего фабриканта, спасшего во время Второй мировой войны почти 1200 евреев." });
-
-            // string result = string.Join(", ", catalog.Select(f => f.Year));  //КАК ВАРИАНТ
-            //catalog = catalog.OrderBy(f => f.Year).ToList();
-            // string orderedResult = string.Join(", ", catalog.Select(f => f.Year));
+            this.catalogSourse.Add(new Films() { Name = "Список Шиндлера", Genre = new List<string>(){ "драма", "биография", "история" }, Year = 1993 , ImagePath = "Res\\Schindler'sList.jpg", Description = "Фильм рассказывает реальную историю загадочного Оскара Шиндлера, члена нацистской партии, преуспевающего фабриканта, спасшего во время Второй мировой войны почти 1200 евреев." });
             
+          
             List <string> temp = new List<string>();
             this.catalogSourse.ForEach(f => temp.AddRange(f.Genre.ToList()));
-            //.ToList());   
             this.genres.AddRange( temp.Distinct());
             this.years.AddRange(this.catalogSourse.Select(c => c.Year).Distinct().ToList());
             this.years.Sort();
@@ -103,10 +97,7 @@ namespace Kyrsovaya
         {
             Window statwin = new Statistika();
             statwin.Owner = this;
-            string List = "fdsfdfsdf";
-            //this.TopList.Clear();
-            //var ttt = 
-            //const char V = ")"+ "\r\n";
+            string List = "World";
             StringBuilder _Text = new StringBuilder();
             (catalogSourse.Select(c => new { Name = c.Name, ViewCount = c.ViewCount, Year = c.Year}).OrderByDescending(b=> b.ViewCount).Select(v=> v.Name+"("+ v.Year + ") [просмотров - " + v.ViewCount+"]")).ToList().ForEach(c=> _Text.AppendLine(c));
 
@@ -167,7 +158,7 @@ namespace Kyrsovaya
             var getMethod = property.GetGetMethod(true);
             if (getMethod == null)
             {
-                // this shouldn't happen - the expression would reject the property before reaching this far
+               
                 throw new ArgumentException("The referenced property does not have a get method.", "propertyExpression");
             }
 
